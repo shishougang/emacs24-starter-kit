b67cee24d4bf3df1b9b2a4084419241f8e3e9af9
SHI Shougang <shishougang@gmail.com>
2012-07-10 22:17:07 +0800

    add custom things

--- init.el
+++ init.el
@@ -9,6 +9,21 @@
 (setq starter-kit-dir
       (file-name-directory (or load-file-name (buffer-file-name))))
 
+
+(add-to-list 'load-path "~/.emacs.d/src/cedet-1.1/common")
+(require 'cedet)
+(require 'semantic-ia)
+
+;; Enable EDE (Project Management) features
+(global-ede-mode 1)
+ 
+(semantic-load-enable-excessive-code-helpers)
+(semantic-load-enable-semantic-debugging-helpers)
+ 
+;; Enable SRecode (Template management) minor-mode.
+(global-srecode-minor-mode 1)
+
+
 ;; load up the starter kit
 (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))
 
